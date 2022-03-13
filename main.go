1d2b1f4bd9a88252980958a2ecb0194427a11a01
Gustavo <gustavoalex.145@gmail.com>
2022-03-13 11:55:18 -0300

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"banco/contas"
+	"fmt"
+)
+
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	conta.Sacar(valorDoBoleto)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
+func main() {
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
+	
+	fmt.Println(contaExemplo.ObterSaldo())
+
+}
